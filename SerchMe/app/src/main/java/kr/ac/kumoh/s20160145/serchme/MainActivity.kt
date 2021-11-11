ab1e8d956c72f7dc563a0208f61483d6b922ec4a
Gukss <dongkuk97@naver.com>
2021-11-11 13:49:23 +0900

    📝add Login page 📝add viewBinding var

--- SerchMe/app/src/main/java/kr/ac/kumoh/s20160145/serchme/MainActivity.kt
+++ SerchMe/app/src/main/java/kr/ac/kumoh/s20160145/serchme/MainActivity.kt
@@ -2,10 +2,14 @@ package kr.ac.kumoh.s20160145.serchme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20160145.serchme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
     }
 }
