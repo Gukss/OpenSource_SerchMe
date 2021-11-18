3bde340bfec297047d78da4e699a050a9de28e25
Gukss <dongkuk97@naver.com>
2021-11-18 14:03:00 +0900

    📝add page4, add registration.kt, showrecipes.kt files

---
+++ SerchMe/app/src/main/java/kr/ac/kumoh/s20160145/serchme/registration.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20160145.serchme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
---
+++ SerchMe/app/src/main/java/kr/ac/kumoh/s20160145/serchme/showrecipes.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20160145.serchme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class showrecipes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_showrecipes)
+    }
+}
